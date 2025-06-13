d2d1b7095503c0186e55a5c4680ff20558166524
paratonsp <paratonsp@gmail.com>
2025-06-13 15:38:59 +0700

    started

--- elder.config.js
+++ elder.config.js
@@ -35,6 +35,12 @@ module.exports = {
       display: ['errors', 'warnings'], // If the errors are too verbose remove 'warnings'
       //writeLocation: './report.json', // if you want to write a report of errors
     },
+    '@elderjs/plugin-sitemap': {
+      origin: '', // the https://yourdomain.com
+      exclude: [], // an array of permalinks or permalink prefixes. So you can do ['500'] and it will match /500**
+      routeDetails: {}, // set custom priority and change freq if not it falls back to default
+      lastUpdate: {}, // configurable last update for each route type.
+    },
   },
   shortcodes: { closePattern: '}}', openPattern: '{{' },
 };
